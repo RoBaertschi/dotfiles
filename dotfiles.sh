9405d9a29e8eed8679bdc7f07896b14fae93c460
RoBaertschi <rtmbaertschi007@gmail.com>
2025-11-17 21:17:23 +0100

    changes on old system

---
+++ dotfiles.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+set -ex
+
+install_portage_file() {
+    name=$1
+    dir=$(dirname /etc/portage/$name)
+    doas mkdir -p $dir
+    doas cp ./portage/$name /etc/portage/$name
+}
+
+mkdir -p ~/.config
+
+rm -r ~/.config/foot
+cp -r ./foot/ ~/.config/foot
+rm -r ~/.config/sway
+cp -r ./sway/ ~/.config/sway
+rm -r ~/.config/waybar
+cp -r ./waybar/ ~/.config/waybar
+
+install_portage_file package.use/rob-desktop
+install_portage_file package.accept_keywords/rob-desktop
+install_portage_file sets/rob-desktop
+
+repo() {
+    local name=$1
+    doas eselect repository enable $name
+    doas emerge --sync $name
+}
+
+doas emerge -av -1 --noreplace app-eselect/eselect-repository
+repo gentoo-zh
+repo wayland-desktop
+
+install_portage_file sets/rob-desktop
+doas emerge -av @rob-desktop --noreplace
+
+service() {
+    local name=$1
+    doas rc-update add $name default
+    doas rc-service $name start
+}
+
+user_service() {
+    local name=$1
+    rc-update add -U $name default
+    rc-service -U $name start
+}
+
+user_service wireplumber
+user_service pipewire-pulse
